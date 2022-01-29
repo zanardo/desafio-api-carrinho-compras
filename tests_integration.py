30843d48401bfc37c63b68bf07d9f5e96f86a8ee
Antonio Zanardo <zanardo@gmail.com>
2022-01-29 10:10:41 -0300

    adiciona produto no carrinho

--- tests_integration.py
+++ tests_integration.py
@@ -11,7 +11,7 @@ WEBSERVICE = "http://127.0.0.1:5000"
 def api_post(uri: str, dados: Dict) -> Dict:
     "Generaliza um POST na API e retorna os dados"
     log.debug("chamando api via post em %s com dados %s", uri, pformat(dados))
-    res = requests.post(url="{}/{}".format(WEBSERVICE, uri), timeout=5, params=dados)
+    res = requests.post(url="{}/{}".format(WEBSERVICE, uri), timeout=5, data=dados)
     log.debug("retorno do api:\n%s", pformat(res.json()))
     if not res.ok:
         raise ValueError(
@@ -23,6 +23,8 @@ def api_post(uri: str, dados: Dict) -> Dict:
 def main():
     log.info("criando novo carrinho")
     carrinho = api_post("/novo", dados={})["carrinho_codigo"]
+    log.info("adicionando produto no carrinho")
+    api_post("/produto-adiciona", dados={"carrinho": carrinho, "produto": "AB1234567"})
 
 
 if __name__ == "__main__":
