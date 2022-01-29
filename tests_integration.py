3d6a4bebcd67574c8cd21d85eef2205d1d870a03
Antonio Zanardo <zanardo@gmail.com>
2022-01-29 10:04:52 -0300

    iniciando testes de integração

---
+++ tests_integration.py
@@ -0,0 +1,29 @@
+from pprint import pformat
+from typing import Dict
+
+import requests
+
+from api_carrinho.log import log
+
+WEBSERVICE = "http://127.0.0.1:5000"
+
+
+def api_post(uri: str, dados: Dict) -> Dict:
+    "Generaliza um POST na API e retorna os dados"
+    log.debug("chamando api via post em %s com dados %s", uri, pformat(dados))
+    res = requests.post(url="{}/{}".format(WEBSERVICE, uri), timeout=5, params=dados)
+    log.debug("retorno do api:\n%s", pformat(res.json()))
+    if not res.ok:
+        raise ValueError(
+            "erro ao carregar dados da api: %s: %s", res.status_code, res.text
+        )
+    return res.json()["dados"]
+
+
+def main():
+    log.info("criando novo carrinho")
+    carrinho = api_post("/novo", dados={})["carrinho_codigo"]
+
+
+if __name__ == "__main__":
+    main()
